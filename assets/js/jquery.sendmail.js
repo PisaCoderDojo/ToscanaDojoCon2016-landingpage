3b4da07faf485a3a8fd51e7af20b23e48340b24f
dido18 <davideneri18@gmail.com>
2016-06-01 19:08:13 +0200

    loading and response mail

--- assets/js/jquery.sendmail.js
+++ assets/js/jquery.sendmail.js
@@ -9,15 +9,28 @@ $(document).ready( function(){
         body: $("#contact-body").val()
       };
       console.log(data);
+
+
       if(data.body && data.mail && isValidEmailAddress(data.mail)){
+          $("#contact-form").hide();
+          $("#contact-loading").show();
+          $('#contact-error').text('')
           $.post('/php/sendMail.php', data, function(data) {
             if(data=="true"){
               console.log("Mail inviata")
+              $("#contact-loading").hide();
+              $("#contact-response").show();
             }
             else {
+              $("#contact-loading").show();
+                $("#contact-form").show();
+                $('#contact-error').text('Si è verificato un errore')
                 console.log("errore di invio")
               }
           })
+      }
+      else{
+        $('#contact-error').text("Error: Email non valida o vuota")
       }
   }
 )});
